70095b9bb865d23c4dd0b673f7fe0b5f1d29628e
kenny caldieraro <kenny.caldieraro@gmail.com>
2023-05-30 18:43:31 +0200

    fix menu

--- public/themes/wordplate/index.php
+++ public/themes/wordplate/index.php
@@ -2,7 +2,6 @@
 
 $context = \Timber\Timber::context();
 $context['posts'] = new \Timber\PostQuery();
-$context['categories'] = \Timber\Timber::get_terms('category');
 $context['prev'] = true;
 
 \Timber\Timber::render('pages/index.twig', $context);
